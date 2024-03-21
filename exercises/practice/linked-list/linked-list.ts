d961e179ea58cc5e1b6e54d747ba2d6dae76f822
chenuvi <chenuvi@outlook.com>
2024-03-21 23:44:47 +0800

    Feat:linked-list-1

--- exercises/practice/linked-list/linked-list.ts
+++ exercises/practice/linked-list/linked-list.ts
@@ -1,25 +1,63 @@
+type Node<T> = {
+  head: Node<T>;
+  value: T;
+  tail: Node<T>;
+} | null;
+
 export class LinkedList<TElement> {
-  public push(element: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  last: Node<TElement> = null;
+  first: Node<TElement> = null;
+
+  public initialItem(element: TElement) {
+    const el = {
+      head: null,
+      value: element,
+      tail: null,
+    };
+    this.last = el;
+    this.first = el;
   }
 
-  public pop(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public push(element: TElement) {
+    if (!this.last) {
+      this.initialItem(element);
+      return;
+    }
+    const el = {
+      head: this.last,
+      value: element,
+      tail: null,
+    };
+    this.last.tail = el;
+    this.last = el;
+  }
+
+  public pop() {
+    if (!this.last) return null;
+
+    const toBeReturned = this.last.value;
+    this.last = this.last.head;
+    if (this.last) {
+      this.last.tail = null;
+    } else {
+      this.first = null;
+    }
+    return toBeReturned;
   }
 
   public shift(): unknown {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public unshift(element: unknown) {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public delete(element: unknown) {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public count(): unknown {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 }
