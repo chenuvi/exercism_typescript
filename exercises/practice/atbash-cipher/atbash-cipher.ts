5dc88aa282701b3df02ce7d0a67e75ecf6380b22
chenuvi <chenuvi@outlook.com>
2024-03-27 23:45:32 +0800

    Feat: atbash-cipher

--- exercises/practice/atbash-cipher/atbash-cipher.ts
+++ exercises/practice/atbash-cipher/atbash-cipher.ts
@@ -1,7 +1,18 @@
-export function encode(plainText: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const ALPHA = "abcdefghijklmnopqrstuvwxyz";
+
+export function encode(plainText: string): string {
+  return transcode(plainText).reduce(
+    (prev, curr, i) => (i === 0 || i % 5 ? prev + curr : prev + " " + curr),
+    ""
+  );
 }
 
-export function decode(cipherText: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function decode(cipherText: string): string {
+  return transcode(cipherText).join("");
+}
+
+function transcode(text: string) {
+  return Array.from(text.replace(/\W/g, "").toLowerCase()).map((c) =>
+    c.match(/\d/) ? c : ALPHA.charAt(25 - ALPHA.indexOf(c))
+  );
 }
