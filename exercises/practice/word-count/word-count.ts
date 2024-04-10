8942cbdd3c7120331069791eb3f7b80e4c2a3a1b
chenuvi <chenuvi@outlook.com>
2024-04-10 23:35:35 +0800

    Feat: word count

--- exercises/practice/word-count/word-count.ts
+++ exercises/practice/word-count/word-count.ts
@@ -1,3 +1,9 @@
-export function count() {
-  throw new Error('Remove this statement and implement this function')
+export function count(words: string) {
+  return words
+    .toLocaleLowerCase()
+    .match(/\b([\w']+)\b/g)
+    ?.reduce(
+      (counts, word: string) => counts.set(word, (counts.get(word) ?? 0) + 1),
+      new Map<string, number>()
+    );
 }
