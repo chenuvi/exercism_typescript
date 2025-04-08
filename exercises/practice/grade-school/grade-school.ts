4197309cf195a2a127b89289a3fc279e11c04e5a
chenuvi <chenuvi@outlook.com>
2025-04-08 22:42:28 +0800

    feat: Grade School 1

--- exercises/practice/grade-school/grade-school.ts
+++ exercises/practice/grade-school/grade-school.ts
@@ -1,13 +1,27 @@
+type Student = string;
+type Grade = number;
+// type StudentRooster = Record<string, Student[]>;
+type StudentGrades = Map<Student, Grade>;
+
 export class GradeSchool {
+  private students: StudentGrades;
+
+  constructor() {
+    this.students = new Map();
+  }
+
   roster() {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  add(student: Student, level: Grade) {
+    this.students.set(student, level);
   }
 
-  grade() {
-    throw new Error('Remove this statement and implement this function')
+  grade(level: Grade): Student[] {
+    return Array.from(this.students.entries())
+      .filter(([, studentGrade]) => studentGrade === level)
+      .map(([student]) => student)
+      .sort();
   }
 }
